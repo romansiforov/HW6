d1b3bee250012b5f47f4e8ef382bb5b1b888314a
Roman <roman@defentry.com>
2023-06-01 17:34:34 +0300

    HW with toast

--- cypress/e2e/testWithToast.cy.js
+++ cypress/e2e/testWithToast.cy.js
@@ -1,5 +1,4 @@
 /// <reference types="cypress"/>
-
 describe('Tests with toast', ()=>{
     const tests = [
         {
@@ -18,8 +17,18 @@ describe('Tests with toast', ()=>{
             cy.get('.col-md-6.col-sm-12:nth-child(2) .form-group button.select-button').click();
             cy.get('nb-option[ng-reflect-value="danger"]').click();
             cy.get('.col-md-6.col-sm-12:nth-child(2) .form-group button.select-button').should('contain', 'danger');
-
-
+            cy.get('.col-md-6.col-sm-12:nth-child(1) .form-group button.select-button').click();
+            cy.get('nb-option[ng-reflect-value="top-right"]').click();
+            cy.get('.col-md-6.col-sm-12:nth-child(1) .form-group button.select-button').should('contain', 'top-right');
+            cy.get('input[name="timeout"]').clear().type(20000);
+            cy.get('.mat-ripple.appearance-filled.size-medium.shape-rectangle.status-basic.nb-transition').click();
+            cy.get('.ng-tns-c209-54.ng-star-inserted nb-toast .title.subtitle').should('contain', title);
+            cy.get('.ng-tns-c209-54.ng-star-inserted nb-toast .message').should('contain', content);
+            cy.get('.ng-tns-c209-54.ng-star-inserted nb-toast').should('have.css', 'background-color').and('eq', 'rgb(176, 0, 32)');
+            cy.get('.ng-tns-c209-54.ng-star-inserted nb-toast').should('have.prop', 'clientLeft', 1);
+            cy.get('.ng-tns-c209-54.ng-star-inserted nb-toast').should('have.prop', 'clientTop', 1);
+            cy.get('.ng-tns-c209-54.ng-star-inserted nb-toast path[d="M11.11 23a1 1 0 0 1-.34-.06 1 1 0 0 1-.65-1.05l.77-7.09H5a1 1 0 0 1-.83-1.56l7.89-11.8a1 1 0 0 1 1.17-.38 1 1 0 0 1 .65 1l-.77 7.14H19a1 1 0 0 1 .83 1.56l-7.89 11.8a1 1 0 0 1-.83.44zM6.87 12.8H12a1 1 0 0 1 .74.33 1 1 0 0 1 .25.78l-.45 4.15 4.59-6.86H12a1 1 0 0 1-1-1.11l.45-4.15z"]');
+            
         });
     });
 });
